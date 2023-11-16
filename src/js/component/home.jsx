b8b79b1accc5e672c7b2de5169d3f72d5cbea37c
Tony1406 <142957069+Tony1406@users.noreply.github.com>
2023-11-16 00:07:53 +0000

    Landing page

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,23 +2,28 @@ import React from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
+import Navbar from "./navbar.jsx";
+import Jumbotron from "./jumbotron.jsx";
+import Card from "./card.jsx";
 
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="text-center container">
+			<Navbar />
+			<Jumbotron />
+			<br/>
+			<div className="row">
+				<div className="col">
+					<Card imagen="https://static.guiainfantil.com/media/40268/c/sudamina-granitos-en-los-bebes-por-el-calor-md.jpg" titulo="bebe"/>
+				</div>
+				<div className="col">
+					<Card imagen="https://estaticos-cdn.prensaiberica.es/clip/823f515c-8143-4044-8f13-85ea1ef58f3a_16-9-discover-aspect-ratio_default_0.jpg" titulo="perro"/>
+				</div>
+				<div className="col">
+					<Card />
+				</div>
+			</div>
 		</div>
 	);
 };
